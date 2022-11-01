bafdd562e00fabfb4dfbe718acb61c87bf470e17
Pankaj Parkar <parkarpankaj3@gmail.com>
2022-11-01 22:39:53 +0800

    Exercise 1 Product List finished

---
+++ src/app/models/product.model.ts
@@ -0,0 +1,8 @@
+export interface ProductModel {
+  readonly id: number;
+  readonly title: string;
+  readonly price: number;
+  readonly category: string;
+  readonly description: string;
+  readonly image: string;
+}
