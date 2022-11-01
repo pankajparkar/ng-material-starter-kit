df32ebf79133ba8a2fcedef79409ad95f5dc2b7a
Pankaj Parkar <parkarpankaj3@gmail.com>
2022-11-01 23:10:34 +0800

    Exercise 2 Add Product Form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { AddProductComponent } from './components/add-product/add-product.component';
 import { ProductListComponentModule } from './components/product-list/product-list.component-module';
 import { ProductsServiceModule } from './services/products.service-module';
+import { AddProductComponentModule } from './components/add-product/add-product.component-module';
 
 @NgModule({
-  imports: [RouterModule.forRoot([{ path: 'product', component: ProductListComponent }]), ProductListComponentModule, ProductsServiceModule],
+  imports: [
+    RouterModule.forRoot([
+      { path: 'product', component: ProductListComponent },
+      { path: 'add-product', component: AddProductComponent }
+    ]),
+    ProductListComponentModule,
+    ProductsServiceModule,
+    AddProductComponentModule,
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
